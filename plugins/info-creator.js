28c52d1077b7bf506b75e9f6f72ff2f3d83ea3d7
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-10 21:19:13 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -10,7 +10,7 @@ VERSION:3.0
 N:Sy;Bot;;;
 FN: TioXd
 item.ORG: Creator Bot
-item1.TEL;waid=6282221792667:6282221792667@s.whatsapp.net
+item1.TEL;waid=62859106985947:62859106985947@s.whatsapp.net
 item1.X-ABLabel:Nomor Creator Bot / Owner Bot
 item2.EMAIL;type=INTERNET:Kalo Chat Gak Penting Gak Akan Di Balas.
 item2.X-ABLabel:Email
